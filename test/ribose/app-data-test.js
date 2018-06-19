15eba2a626ce2583f2dd801cc334f14074215195
Abu Nashir <abunashir@gmail.com>
2018-06-19 19:53:27 +0200

    Add request client with validations

--- test/ribose/app-data-test.js
+++ test/ribose/app-data-test.js
@@ -5,7 +5,7 @@ describe("App Data", () => {
   describe(".all", () => {
     it("retrieves the list of app data", () => {
       ribose.authenticate({token: "secret-token", email: "api@ribose.test" })
-      app_data = ribose.appData.getAll();
+      app_data = ribose.appData.getAll({actions: "hello"});
 
       console.log(app_data);
 
